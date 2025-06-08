39ef772250beb6a4c6d693bf0b9eba6af80c1d38
pratiksagare <79927151+pratiksagare@users.noreply.github.com>
2025-06-08 21:48:52 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { useContext } from "react";
 import { Layout } from "./Layout";
-import { GlobalContext } from "./lib/context/GlobalContext";
 
 export default function App() {
-  const { GlobalContextProvider } = useContext(GlobalContext)
   return (
     <Layout />
   )
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,15 +4,22 @@ import Container from "./lib/components/Container";
 
 export const Layout = () => {
     return (
-        <div className="w-full min-w-[450px] overflow-y h-screen flex flex-col bg-[#181818] text-white">
-            <Menubar />
-            <div className="flex-1 flex">
-                <div className="bg-[#181818] border-r border-[#3C3C3C]">
-                    <Sidebar />
+        <div>
+            <div className="w-full hidden md:flex min-w-[450px] overflow-y h-screen flex flex-col bg-[#181818] text-white">
+                <Menubar />
+                <div className="flex-1 flex">
+                    <div className="bg-[#181818] border-r border-[#3C3C3C]">
+                        <Sidebar />
+                    </div>
+                    <div className="flex-1 h-full">
+                        <Container />
+                    </div>
                 </div>
-                <div className="flex-1 h-full">
-                    <Container />
-                </div>
+            </div>
+            <div className="flex justify-center items-center md:hidden w-full px-4 h-screen bg-[#181818] text-white">
+                <span className="font-mono">
+                    This application is best viewed on a desktop or laptop. Please switch to a larger screen for optimal experience.
+                </span>
             </div>
         </div>
     );
